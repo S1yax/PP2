f44de4704b7693a7d9ce0942b3511d4ba3b53973
Saniya Niyazkhan <s_niyazkhan@kbtu.kz>
2025-02-19 01:02:45 +0500

    lab2

---
+++ lab2/fourth.py
@@ -0,0 +1,11 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(len(thistuple))
+
+thostuple = ("apple")
+print(type(thostuple))
+
+print(thistuple[1])
+
+for i in range(len(thistuple)):
+  print(thistuple[i])
